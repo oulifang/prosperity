7498a7dcef9a1178df008333b5d7c9432581570c
oulifang <1842459310@qq.com>
2018-08-13 16:24:56 +0800

    vueapp

---
+++ vueapp/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app"> 
+  <CommonHeader :selectMenu="selectMenu"></CommonHeader>
+    <router-view class="content"/>
+   <CommonFooter @changeTitle="changeTitle" :menuList="menuList"></CommonFooter>
+  </div>
+</template>
+
+<script>
+  import CommonHeader from '@/components/CommonHeader.vue';
+  import CommonFooter from '@/components/CommonFooter.vue';
+  export default{
+     comments:{
+      CommonHeader,
+      CommonFooter
+     },
+     methods: {
+    changeTitle(menu){
+      this.selectMenu = menu;
+        }
+     },
+     data(){
+       return{
+         menuList:[
+           {
+             title:'电影',
+             bgColor:'#e54847',
+             name:'movie',
+             path:'/movie'
+           },{
+             title:'音乐',
+             bgColor:'#e54847',
+             name:'music',
+             path:'/music'
+           },{
+             title:'书籍',
+             bgColor:'#e54847',
+             name:'book',
+             path:'/book'
+           },{
+             title:'图片',
+             bgColor:'#e54847',
+             name:'photo',
+             path:'/photo'
+           }
+         ],
+         selectMenu:{} //当前选中的菜单
+       }
+     }
+  }
+ 
+</script>
+<style lang="scss">
+.content{
+  margin: 1rem 0;
+}
+</style>
