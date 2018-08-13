4c9daec84f4252133a719917c4377cc638e7e2cd
oulifang <1842459310@qq.com>
2018-08-13 11:06:32 +0800

    vuecli

---
+++ vuecli/src/views/Movie.vue
@@ -0,0 +1,48 @@
+<template>
+    <ul>
+        <li @click="showDetail(movie)" v-for="movie in movielist" :key="movie.id">
+            <!-- <router-link :to="'/moviedetail/'+movie.id"> -->
+                {{movie.name}}
+            <!-- </router-link>
+             -->
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            movielist:[
+            {
+                id:1,
+                name:"爱情公寓"
+            },
+            {
+                id:2,
+                name:"一出好戏"
+            },{
+                id:3,
+                name:"狄仁杰4"
+            },{
+                id:4,
+                name:"蚁人2"
+            },{
+                id:5,
+                name:"巨齿鲨"
+            }
+        ]
+        }; 
+    },
+    methods:{
+        showDetail(movie){
+            this.$router.push('/moviedetail/'+movie.id);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ vuecli/src/views/MovieDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>电影详情列表哦</h1>
+        <h1>电影Id:{{$route.params.movieId}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            detail:{}
+        }
+    },
+    created() {
+        let movieId = this.$route.params.movieId;
+    },
+}
+</script>
