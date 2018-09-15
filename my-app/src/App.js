559eb30d0239ee11cdacff6b8f855513801fe26f
oulifang <1842459310@qq.com>
2018-09-15 14:54:49 +0800

    my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as router,Router,Link} from 'component';
 
 class App extends Component {
   render() {
