9c47c9816f0d5f53b24198d2943c7e8621056f4f
oulifang <1842459310@qq.com>
2018-09-16 14:27:26 +0800

    my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import {BrowserRouter as router,Router,Link} from 'component';
+import Class1 from './component/Class1';
+import Class2 from './component/Class2';
+import Class3 from './component/Class3';
+import Class4 from './component/Class4';
+import {
+   BrowserRouter as Router, 
+   Route, 
+   Link ,
+   Switch
+  } from "react-router-dom";
 
+const NoMatch = ()=><h1>404</h1>  
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <Link to="/class1">class1</Link>
+          <Link to="/class2">class2</Link>
+          <Link to="/class3">class3</Link>
+          <Link to="/class4">class4</Link>
+          <Switch>
+            <Route exact path="/class1" component={Class1}/>
+            <Route path="/class2" component={Class2}/>
+            <Route path="/class3" component={Class3}/>
+            <Route path="/class4" component={Class4}/>
+            <Route component={NoMatch}/>
+          </Switch>
+        </div>
       </div>
+      </Router>
     );
   }
 }
