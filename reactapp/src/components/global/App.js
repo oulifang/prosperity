dfd967f3c9cb1d87d0d2dd151fd729e05dbdb751
oulifang <1842459310@qq.com>
2018-09-23 16:45:09 +0800

    reactapp

--- reactapp/src/components/global/App.js
+++ reactapp/src/components/global/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
+import 'antd-mobile/dist/antd-mobile.css';
 import './../../assets/css/App.css';
-import Test from '../../Test'
-import NavBar from '../../NavBar'
-import { Carousel, WingBlank } from 'antd-mobile';
-// import img from './../../assets/img/dianying.png'
+import Home from './../home/Home';
+import Detail from './../detail/Detail'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 class App extends Component {
-
   render() {
-    let img = "dianying";
     return (
-      <div className="App">
-      <NavBar/>
-
-        <img src={require(`./../../assets/img/${img}.png`)} alt=""/>
-        {/* <header className="App-header">
-        </header>
-        <p className="App-intro">
-        </p> */}
-        <Test/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home}/>
+          <Route path="/detail/:id" component={Detail}/>
+        </div>
+      </Router>
     );
   }
 }
